5a9335b79850da08e0d87124107b2ef364eec245
lisamcgerr <lisamarie.mcgerr@gmail.com>
2022-12-05 15:12:22 -0600

    installed material-ui and icon import in Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import '../style/Sidebar.css'
+import '../style/Sidebar.css';
+import { Avatar } from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-
+        <div className="sidebar__header">
+            <Avatar />
+            <div className="sidebar__headerRight">
+                <DonutLargeIcon />
+                <ChatIcon />
+                <MoreVertIcon />
+            </div>
+        </div>
+        <div className="sidebar__search"></div>
+        <div className="sidebar__chats"></div>
     </div>
   )
 }
