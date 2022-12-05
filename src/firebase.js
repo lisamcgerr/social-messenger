a79ddb11d284bab6913409cc3862e2a66c0b743a
lisamcgerr <lisamarie.mcgerr@gmail.com>
2022-12-05 13:46:39 -0600

    initial cleanup & firebase config added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClXRHPJI4tFGD5C-PvtXwthdOPr-svTQY",
+    authDomain: "social-messenger-4ac0c.firebaseapp.com",
+    projectId: "social-messenger-4ac0c",
+    storageBucket: "social-messenger-4ac0c.appspot.com",
+    messagingSenderId: "1041589060789",
+    appId: "1:1041589060789:web:d01d092cd0dc13bb3f2afc"
+};
+
+//@TODO - missing measurementId
