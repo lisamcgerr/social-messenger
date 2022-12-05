e36db7d9727e9c581e637bdfd09aa9bb90e78790
lisamcgerr <lisamarie.mcgerr@gmail.com>
2022-12-05 14:58:37 -0600

    Sidebar.js &.css added, App.css added, modifications to initial create react app

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../style/App.css';
+import Sidebar from './Sidebar';
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/Sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+
+    </div>
+  )
+}
+
+export default Sidebar;
