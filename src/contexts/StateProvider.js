a91970bb98620490f29a016744fb295f80a53e5f
lisamcgerr <lisamarie.mcgerr@gmail.com>
2022-12-22 13:25:15 -0600

    onAuthStateChange added

--- src/contexts/StateProvider.js
+++ src/contexts/StateProvider.js
@@ -1,4 +1,7 @@
-import React, { createContext, useContext, useReducer } from 'react';
+import React, { createContext, useContext, useReducer, useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../components/firebase';
+
 
 // @TODO remove
 // this is the data layer
@@ -7,11 +10,24 @@ export const StateContext = createContext();
 // @TODO remove
 // build a provider
 // children is referring to App.js in index.js
-export const StateProvider = ({ reducer, initialState, children }) => (
-    <StateContext.Provider value={useReducer(reducer, initialState)} >
-        {children}
-    </StateContext.Provider>
-)
+export const StateProvider = ({ reducer, initialState, children }) => {
+    const [ user, setUser ] = useState({});
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('currentUser: ', currentUser); // @TODO delete
+            setUser(currentUser);
+        });
+        return () => {
+            unsubscribe();
+        };
+    }, [])
+
+    return (
+        <StateContext.Provider value={useReducer(reducer, initialState)} >
+            {children}
+        </StateContext.Provider>
+)}
 
 // @TODO remove
 // pulling from the data layer - used inside of a component
